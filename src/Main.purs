de7bfb9f09279ed3abab1c46f1eda2b58e37c717
Maciej Smolinski <contact@maciejsmolinski.com>
2016-08-04 22:27:05 +0200

    Modules: Move actual data to Data module.

--- src/Main.purs
+++ src/Main.purs
@@ -1,29 +1,10 @@
 module Main where
 
 import Prelude
-import Data.Maybe (Maybe(..))
+import Data (person1, person2, maybePerson1, maybePerson2, maybePerson3)
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
-import Types (Person(..))
 
-createPerson :: String -> Int -> Person
-createPerson name age =
-  Person { name: name, age: age }
-
-person1 :: Person
-person1 = createPerson "John" 18
-
-person2 :: Person
-person2 = createPerson "Sam" 20
-
-maybePerson1 :: Maybe Person
-maybePerson1 = createPerson <$> Just "Joe" <*> Nothing
-
-maybePerson2 :: Maybe Person
-maybePerson2 = createPerson <$> Nothing <*> Just 27
-
-maybePerson3 :: Maybe Person
-maybePerson3 = createPerson <$> Just "Joe" <*> Just 27
 
 main :: forall e. Eff (console :: CONSOLE | e) Unit
 main = do
