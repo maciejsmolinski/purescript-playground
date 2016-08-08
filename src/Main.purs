8a57a378a229de3c26b3a3c52069249de9b25583
Maciej Smolinski <contact@maciejsmolinski.com>
2016-08-08 18:29:44 +0200

    Examples: Use GUI example as the main one.

--- src/Main.purs
+++ src/Main.purs
@@ -1,19 +1,32 @@
 module Main where
 
 import Prelude
-import Person.Api as Person
-import RockPaperScissors.Api as RockPaperScissors
+
+-- -- GUI Example:
+
+import RockPaperScissorsGUI.Main as RPSG
 import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Console (CONSOLE, log)
-import Control.Monad.Eff.Random (RANDOM)
+import Halogen as H
 
-newline :: forall e. Eff (console :: CONSOLE | e) Unit
-newline = log $ "\n" <> "************************" <> "\n"
+main :: Eff (H.HalogenEffects ()) Unit
+main = RPSG.main
 
-main :: forall e. Eff (console :: CONSOLE, random :: RANDOM | e) Unit
-main = do
-  newline
-  Person.print
 
-  newline
-  RockPaperScissors.play
+-- -- Console Examples:
+
+-- import Person.Api as Person
+-- import RockPaperScissors.Api as RockPaperScissors
+-- import Control.Monad.Eff (Eff)
+-- import Control.Monad.Eff.Console (CONSOLE, log)
+-- import Control.Monad.Eff.Random (RANDOM)
+--
+-- newline :: forall e. Eff (console :: CONSOLE | e) Unit
+-- newline = log $ "\n" <> "************************" <> "\n"
+--
+-- main :: forall e. Eff (console :: CONSOLE, random :: RANDOM | e) Unit
+-- main = do
+--   newline
+--   Person.print
+--
+--   newline
+--   RockPaperScissors.play
