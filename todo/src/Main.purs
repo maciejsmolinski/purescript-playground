8b5a81bc95f0d59e3c6f9db3d168298f7b12df8f
Maciej Smolinski <contact@maciejsmolinski.com>
2017-07-21 18:34:14 +0200

    Todo: Use the same html todo elm app uses.

--- todo/src/Main.purs
+++ todo/src/Main.purs
@@ -4,10 +4,17 @@ import Prelude
 
 import App.Main as AM
 import Control.Monad.Eff (Eff)
+import Control.Monad.Eff.Exception (error)
+import Control.Monad.Error.Class (throwError)
+import DOM.Node.ParentNode (QuerySelector(..))
+import Data.Maybe (Maybe(..))
 import Halogen.Aff as HA
 import Halogen.VDom.Driver (runUI)
 
 main :: Eff (HA.HalogenEffects ()) Unit
 main = HA.runHalogenAff do
-  body <- HA.awaitBody
-  runUI AM.main unit body
+  HA.awaitLoad
+  element <- HA.selectElement $ QuerySelector "[data-module='Main']"
+  case element of
+    Nothing -> throwError (error "Could not find [data-module='Main']")
+    Just node -> runUI AM.main unit node
