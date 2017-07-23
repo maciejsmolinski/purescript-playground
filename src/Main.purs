1c937dc823f4150550732c8332934b1a457fc5b3
Maciej Smolinski <contact@maciejsmolinski.com>
2017-07-23 20:05:09 +0200

    Playground: Removing old examples.

--- src/Main.purs
+++
@@ -1,9 +0,0 @@
-module Main where
-
-import Prelude
-import RockPaperScissorsGUI.Main as RPSG
-import Control.Monad.Eff (Eff)
-import Halogen as H
-
-main :: Eff (H.HalogenEffects ()) Unit
-main = RPSG.main
